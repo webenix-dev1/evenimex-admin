95cdbb49955813b004fb3cd1c35e3d231b3def08
Devraj Rangani <devrajrangani@Devrajs-MacBook-Air.local>
2022-04-20 21:52:30 +0530

    remove aws key

--- Services/S3.js
+++ Services/S3.js
@@ -1,9 +1,7 @@
 console.log("Process ::", process.env.NEXT_AWS_ACCESS_KEY);
 const S3 = {
-  accessKeyId: process.env.NEXT_AWS_ACCESS_KEY || "AKIAYKZO5MLV5FYRPT3Z",
-  secretAccessKey:
-    process.env.NEXT_AWS_SECRET_KEY ||
-    "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",
+  accessKeyId: process.env.NEXT_AWS_ACCESS_KEY,
+  secretAccessKey: process.env.NEXT_AWS_SECRET_KEY,
 };
 
 export default S3;
